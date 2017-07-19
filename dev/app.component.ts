e697a3f53711db20906f85005ef41784ec97f4d7
prabudda <pfe@tiqri.com>
2017-07-19 10:41:29 +0530

    replace the commit 0000000

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -14,3 +14,21 @@ import {MyComponentComponent} from "./my-component.component";
 export class AppComponent {
   title = 'My First Angular App';
 }
+
+
+import {Component} from 'angular2/core';
+import {MyComponentComponent} from "./my-component.component";
+
+@Component({
+    selector: 'app',
+    template: `
+        <h1>Angular 2 {{title}}</h1>
+        <p>Now comes second Components,-</p>
+        <my-component></my-component>
+    `,
+  directives:[MyComponentComponent]
+})
+
+export class AppComponent {
+  title = 'My First Angular App';
+}
